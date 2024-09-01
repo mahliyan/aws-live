7650889755c861f826036980ec6623723ff6e0b3
mahliyan <12myyan09@gmail.com>
2024-09-01 20:06:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.clc3y4qkncct.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "cloudcomputing"
+customdb = "employee"
+custombucket = "mahly-employee"
+customregion = "us-east-1"
+
